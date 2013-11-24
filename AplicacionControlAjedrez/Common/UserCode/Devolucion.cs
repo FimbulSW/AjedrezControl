6ebc510b647f7a41fe34d4652e2336a2dee01e2c
FimbulSW <fimbul_invierno_eterno@hotmail.com>
2013-11-23 22:13:57 -0600

    Base control de ajedrez LSL

---
+++ AplicacionControlAjedrez/Common/UserCode/Devolucion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class Devolucion
+    {
+        partial void Prestamista_Compute(ref string result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            result = this.Application.User.Name;
+        }
+
+        partial void Monto_Compute(ref decimal result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            // TODO: Establecer cómo se realizará el tipo de cobro.
+            result = 0;
+        }
+
+        partial void HoraDevolucion_Compute(ref DateTime result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            result = DateTime.Now;
+
+        }
+    }
+}
---
+++ AplicacionControlAjedrez/Common/UserCode/Prestamo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class Prestamo
+    {
+        public static readonly int HoraMaxima = 18;
+        public static readonly int TiempoPrestamo = 2;
+
+        partial void Prestamista_Compute(ref string result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            result = this.Application.User.Name;
+
+        }
+
+        partial void LimiteDevolucion_Compute(ref DateTime result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            //Se
+            result = this.HoraPrestamo.AddHours(TiempoPrestamo);
+            if(result.Hour >= HoraMaxima)
+            {
+                var now = DateTime.Now;
+                result = new DateTime(now.Year, now.Month, now.Day, HoraMaxima, 0, 0);
+            }
+        }
+
+        partial void HoraPrestamo_Compute(ref DateTime result)
+        {
+            // Establece el resultado en el valor del campo deseado
+            result = DateTime.Now;
+        }
+    }
+}
